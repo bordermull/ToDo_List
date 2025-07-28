d8ac36548357fd857656e714ad28b30a16717d95
bordermull <krokodil.jr@yandex.ru>
2025-07-28 15:37:53 +0400

    new logic

--- src/ToDoListApp.java
+++ src/ToDoListApp.java
@@ -6,5 +6,8 @@ public class ToDoListApp {
         System.out.println("Список дел");
         Scanner scanner = new Scanner(System.in);
         boolean run = true;
+        while (run) {
+            System.out.println("Меню\n 1. Добавить задачу\n 2. Удалить задачу\n 3. Изменить задачу\n 4. Показать задачи\n 5. Выход");
+        }
     }
 }
