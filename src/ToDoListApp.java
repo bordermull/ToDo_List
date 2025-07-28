c78a479de20374d0afa8d365dd73e896f74528dc
bordermull <krokodil.jr@yandex.ru>
2025-07-28 15:31:21 +0400

    new logic

---
+++ src/ToDoListApp.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ToDoListApp {
+    public static void main(String[] args) {
+
+        System.out.println("Список дел");
+        Scanner scanner = new Scanner(System.in);
+        boolean run = true;
+    }
+}
